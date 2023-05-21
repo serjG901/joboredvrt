c416345b45eb7843bde7414bbe36fb89a463e999
serjG901 <skw777@mail.ru>
2023-05-22 00:03:08 +0300

    Update ErrorSearchData.tsx

--- src/components/thing/error-search-data/ErrorSearchData.tsx
+++ src/components/thing/error-search-data/ErrorSearchData.tsx
@@ -1,8 +1,7 @@
 import useLanguageStore from "../../../store/useLanguageStore";
 import ErrorSearch from "../../substance/error-search";
-import Image from "./error.svg";
 
 export default function ErrorSearchData() {
   const text: string = useLanguageStore((state) => state.textes.errorSearch);
-  return <ErrorSearch image={Image} text={text} />;
+  return <ErrorSearch text={text} />;
 }
